6b4e92599c856b2e24a6c1add42065a87c5fdbfa
Dan Hutt <danielanthonyhutt@gmail.com>
2015-02-28 11:23:55 -0500

    Initial commit with program "finished"
    Coursera beginning game programming
    
    Assignment Description
    
    Imagine you’re working on developing a game similar to Scorched Earth, a
    2D tank-fighting game where tanks fire shells hoping to destroy the
    enemy tanks. One of the challenges of the game is that the player has to
    predict where the shell will land after following a parabolic
    trajectory. While you may not have taken physics, the equations are
    straightforward and you should be able to develop a solution that will
    accurately calculate the shell path given a starting shell speed and
    trajectory (angle of fire).

---
+++ trajectory_console/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trajectory_console
+{
+	/// <summary>
+	/// Physics simulator for object trajectory.
+	/// </summary>
+	class Program
+	{
+		/// <summary>
+		/// Effect of gravity in m/s
+		/// </summary>
+		public const float gravity = 9.8f;
+
+		/// <summary>
+		/// Main entry point for the application.
+		/// </summary>
+		/// <param name="args">command line arguments.</param>
+		static void Main(string[] args)
+		{
+			//Welcome message
+			Console.WriteLine("Welcome to the physics simulator for object trajectory.");
+			Console.WriteLine(
+				"This will calculate the" +
+				"maximum height of the shell and " +
+				"the distance it will travel");
+
+			//Prompt for input
+			Console.Write("Initial angle in degrees: ");
+			float theta = AngleHelper.ToRadians(float.Parse(Console.ReadLine()));
+
+			Console.Write("Initial speed: ");
+			var speed = float.Parse(Console.ReadLine());
+			float vox = speed * (float)Math.Cos(theta);
+			float voy = speed * (float)Math.Sin(theta);
+
+			//Calculate variables
+
+			//Time to apex
+			float time = voy / gravity;
+
+			//Height at apex
+			float height = (float)Math.Pow(voy, 2) / (2 * gravity);
+
+			//distance horizontally, assuming elevations are equal
+			float distance = vox * 2 * time;
+
+			//print values
+			Console.WriteLine("\nHeight at apex: " + height);
+			Console.WriteLine("Distance travelled: " + distance);
+
+			Console.ReadKey(true);
+		}
+	}
+
+	/// <summary>
+	/// Helps with angle conversions
+	/// </summary>
+	public static class AngleHelper
+	{
+		/// <summary>
+		/// Converts the passed in value to radians.
+		/// </summary>
+		/// <param name="val">angle in degrees.</param>
+		/// <returns>angle in radians</returns>
+		public static float ToRadians(this float val)
+		{
+			return (float)(Math.PI / 180) * val;
+		}
+	}
+}
